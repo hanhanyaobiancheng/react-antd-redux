c1db891b3c010a86a3ed8909409dd95448a1a380
linhan <linhan1991@126.com>
2018-01-22 14:02:12 +0800

    modify some picture loop rule

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Card} from 'antd';
-import {setCurrentUser, getRangeNum} from '../commons/commonFunctions';
+import {setCurrentUser} from '../commons/commonFunctions';
 import styles from  './home.less';
 import a from '../home/1514430994794.jpg';
 import b from './1514431050389.jpg';
@@ -9,11 +9,11 @@ import c from './123123123.jpg';
 import d from './efesfse.jpg';
 import e from './sfggffg.jpg';
 import f from './timg.jpg';
-import g from './timg (4).jpg';
-import h from './timg (5).jpg';
-import i from './timg (6).jpg';
-import j from './timg (7).jpg';
-import k from './timg (8).jpg';
+import g from './timg4.jpg';
+import h from './timg5.jpg';
+import i from './timg6.jpg';
+import j from './timg7.jpg';
+import k from './timg8.jpg';
 
 
 export default class Home extends Component {
@@ -21,14 +21,21 @@ export default class Home extends Component {
         imgUrl: a,
     };
 
+    pictureIndex = 0;
+
     componentDidMount() {
         const backPicUrl = [a, b, c, d, e, f, g, h, i, j, k];
         // const backPicUrl = ['../home/1514430994794.jpg', './1514431050389.jpg'];
 
         setInterval(() => {
-            const num = getRangeNum(0, 10);
-            this.setState({imgUrl: backPicUrl[num]});
-            }, 5000)
+            // const num = getRangeNum(0, 10);
+            console.log(this.pictureIndex);
+            if (this.pictureIndex > 10) {
+                this.pictureIndex = 0;
+            }
+
+            this.setState({imgUrl: backPicUrl[this.pictureIndex++]});
+            }, 3000)
     }
 
 
